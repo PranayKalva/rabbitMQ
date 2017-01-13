092794ff96c49736fb58fc04bedeb456aabd1666
Pranay Kalva <Pranay Kalva@DESKTOP-ADVS6P3>
2017-01-12 21:48:56 -0600

    consumer and producer for hello world

--- syncServices/src/rabbitMQ/Producer.java
+++ syncServices/src/rabbitMQ/Producer.java
@@ -4,7 +4,7 @@ import java.util.concurrent.TimeoutException;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
-public class Client {
+public class Producer {
 	private final static String QUEUE_NAME = "hello"; 
 	public static void main(String[] argv)
 	      throws java.io.IOException, TimeoutException {
