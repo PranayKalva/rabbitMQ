f922a64e3854a528743413776763eec3e439e0d4
Pranay Kalva <Pranay Kalva@DESKTOP-ADVS6P3>
2017-01-12 19:54:52 -0600

    NA

---
+++ syncServices/src/syncServices/Client.java
@@ -0,0 +1,10 @@
+package syncServices;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Client Program Starts here");
+	}
+
+}
